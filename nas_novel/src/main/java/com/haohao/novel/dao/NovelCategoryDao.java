a231798f15968b778105b93863b476941d0c38b7
haohao2333333 <962010144@qq.com>
2022-11-20 23:27:40 +0800

    更新了POM文件

--- nas_novel/src/main/java/com/haohao/novel/dao/NovelCategoryDao.java
+++
@@ -1,7 +0,0 @@
-package com.haohao.novel.dao;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.haohao.domain.novel.NovelCategory;
-
-public interface NovelCategoryDao extends BaseMapper<NovelCategory> {
-}
